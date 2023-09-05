8a23be6a6daeea9baadeecef0f98b16d2d556d97
Kofi Adei <kofidahmed@gmail.com>
2023-09-05 02:07:32 -0400

    Added UI enhancement to header

--- resources/js/Pages/Dashboard/Components/Header.jsx
+++ resources/js/Pages/Dashboard/Components/Header.jsx
@@ -3,9 +3,11 @@ import ApplicationLogo from '@/Components/ApplicationLogo';
 export default function Header() {
     return (
         <header className="x-header bg-purple-200 shadow fixed left-0 top-0 right-0 z-0">
-            <div className="xpense flex items-center">
-                <ApplicationLogo className="block h-9 w-auto" />
-                <span className="ms-0 italic font-bold text-purple-500">XPENSE</span>
+            <div className="w-3/4 px-20">
+                <div className="xpense flex items-center">
+                    <ApplicationLogo className="block h-9 w-auto" />
+                    <span className="ms-0 italic font-bold text-purple-500">XPENSE</span>
+                </div>
             </div>
         </header>
     );
